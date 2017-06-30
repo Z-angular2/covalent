40a24f8cedc0a869ab9bc13fffcf431313e8ec07
emoralesb05 <eduardo.morales@teradata.com>
2017-06-29 18:06:13 -0700

    gh-pages update

---
+++ platform/core/expansion-panel/expansion-panel.module.ts
@@ -0,0 +1,39 @@
+import { Type } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { MdRippleModule, MdIconModule, PortalModule } from '@angular/material';
+
+import { TdExpansionPanelComponent, TdExpansionPanelHeaderDirective, TdExpansionPanelLabelDirective,
+         TdExpansionPanelSublabelDirective, TdExpansionPanelSummaryComponent } from './expansion-panel.component';
+import { TdExpansionPanelGroupComponent } from './expansion-panel-group.component';
+
+const TD_EXPANSION_PANEL: Type<any>[] = [
+  TdExpansionPanelGroupComponent,
+  TdExpansionPanelComponent,
+  TdExpansionPanelHeaderDirective,
+  TdExpansionPanelLabelDirective,
+  TdExpansionPanelSublabelDirective,
+  TdExpansionPanelSummaryComponent,
+];
+
+export { TdExpansionPanelComponent } from './expansion-panel.component';
+export { TdExpansionPanelGroupComponent } from './expansion-panel-group.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MdRippleModule,
+    MdIconModule,
+    PortalModule,
+  ],
+  declarations: [
+    TD_EXPANSION_PANEL,
+  ],
+  exports: [
+    TD_EXPANSION_PANEL,
+  ],
+})
+export class CovalentExpansionPanelModule {
+
+}
